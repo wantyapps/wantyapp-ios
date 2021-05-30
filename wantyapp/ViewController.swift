36d0ea34b1e484362c38f03961f5d5f7659e54c4
wantyapps <56798701+wantyapps@users.noreply.github.com>
2021-05-30 15:49:26 +0300

    Remove comments

--- wantyapp/ViewController.swift
+++ wantyapp/ViewController.swift
@@ -14,15 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
-
-            //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
-            //tap.cancelsTouchesInView = false
-
+        
             view.addGestureRecognizer(tap)
     }
     
     @objc func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
     
